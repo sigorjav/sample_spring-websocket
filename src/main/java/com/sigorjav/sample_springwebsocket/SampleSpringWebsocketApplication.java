8e83da0767e0112c337f445719c4e3d14b3e5b37
sigorjav <sigorjavhan@gmail.com>
2023-06-11 12:27:56 +0900

    first commit

---
+++ src/main/java/com/sigorjav/sample_springwebsocket/SampleSpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.sigorjav.sample_springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigorjav/sample_springwebsocket/SampleSpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigorjav.sample_springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
